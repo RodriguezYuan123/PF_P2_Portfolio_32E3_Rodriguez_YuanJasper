d2eaa3e9be86524cfb78b1f0281dcad436583d27
Oliveros <rodriguezyuanjasp@gmail.com>
2026-03-14 17:13:38 +0800

    www

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Skills from './components/Skills';
-import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 
@@ -13,7 +12,6 @@ function App() {
       <main>
         <Hero />
         <Skills />
-        <Projects />
         <Contact />
       </main>
       <Footer />
